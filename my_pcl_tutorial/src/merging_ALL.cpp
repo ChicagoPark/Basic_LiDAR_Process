fdd895773bfd657c5d77d95b4495d3db8dfeb3be
YOUNGIL PARK <73331241+ChicagoPark@users.noreply.github.com>
2022-02-01 12:43:45 +0900

    Update merging_ALL.cpp

--- my_pcl_tutorial/src/merging_ALL.cpp
+++ my_pcl_tutorial/src/merging_ALL.cpp
@@ -10,14 +10,10 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_livox_m (new pcl::PointCloud<pcl::Poi
 
 int a, b, c, d, e;
 
-//pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_all (new pcl::PointCloud<pcl::PointXYZI> ());
-
 void livox_translation_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg1)
 {
     pcl::PointCloud<pcl::PointXYZI>* source_cloud = new pcl::PointCloud<pcl::PointXYZI>;
     pcl::PointCloud<pcl::PointXYZI>* transformed_cloud = new pcl::PointCloud<pcl::PointXYZI>;
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
     pcl::fromROSMsg (*cloud_msg1, *source_cloud);
     Eigen::Translation3f init_translation(-0.0468, -0.266, 0.0);
     Eigen::AngleAxisf init_rotation_x( 0.0, Eigen::Vector3f::UnitX());
@@ -63,11 +59,7 @@ void livox_merge_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg2)
   pcl::transformPointCloud (*cloud_livox_2, *transformed_cloud, m);
   pcl::copyPointCloud(*transformed_cloud, *cloud_livox_m);
   
-
-
-  //sensor_msgs::PointCloud2 livox_output;
-  //pcl_conversions::fromPCL(*merge_cloud_pc, livox_output);
-
+  // for memory efficiency
   delete merge_cloud;
   delete cloud1_pc;
   delete cloud2_pc;
@@ -76,13 +68,10 @@ void livox_merge_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg2)
 }
 
 
-void 
-cloud_cb3 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg3)
+void cloud_cb3 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg3)
 {
     pcl::PointCloud<pcl::PointXYZI>* source_cloud = new pcl::PointCloud<pcl::PointXYZI>;
     pcl::PointCloud<pcl::PointXYZI>* transformed_cloud = new pcl::PointCloud<pcl::PointXYZI>;
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
     pcl::fromROSMsg (*cloud_msg3, *source_cloud);
     Eigen::Translation3f init_translation(-0.05, 0.52, -0.09);
     Eigen::AngleAxisf init_rotation_x( 0.0, Eigen::Vector3f::UnitX());
@@ -105,13 +94,11 @@ void cloud_cb4 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg4)
 {
     pcl::PointCloud<pcl::PointXYZI>* source_cloud = new pcl::PointCloud<pcl::PointXYZI>;
     pcl::PointCloud<pcl::PointXYZI>* transformed_cloud = new pcl::PointCloud<pcl::PointXYZI>;
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr source_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
-    //  pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZI> ());
     pcl::fromROSMsg (*cloud_msg4, *source_cloud);
     Eigen::Translation3f init_translation(-0.05, -0.52, -0.09);
     Eigen::AngleAxisf init_rotation_x( 0.0, Eigen::Vector3f::UnitX());
     Eigen::AngleAxisf init_rotation_y(0.0, Eigen::Vector3f::UnitY());
-    Eigen::AngleAxisf init_rotation_z(0.0, Eigen::Vector3f::UnitZ()); // 라디안으로 계산해준다.
+    Eigen::AngleAxisf init_rotation_z(0.0, Eigen::Vector3f::UnitZ()); // unit is radian
 
     Eigen::Matrix4f m = (init_translation * init_rotation_x*init_rotation_y* init_rotation_z).matrix();
 
@@ -127,8 +114,6 @@ void cloud_cb4 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg4)
 void cloud_cb5 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg5)
 {
   pcl::fromROSMsg (*cloud_msg5, *cloud_ns2);
-  //pcl::PointCloud<pcl::PointXYZI>* merge_cloud = new pcl::PointCloud<pcl::PointXYZI>;
-
   pcl::PCLPointCloud2* cloud_ns1_pc = new pcl::PCLPointCloud2;
   pcl::PCLPointCloud2* cloud_ns2_pc = new pcl::PCLPointCloud2;
   pcl::PCLPointCloud2* cloud_ns3_pc = new pcl::PCLPointCloud2;
@@ -140,8 +125,6 @@ void cloud_cb5 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg5)
   pcl::toPCLPointCloud2(*cloud_ns2, *cloud_ns2_pc);
   pcl::toPCLPointCloud2(*cloud_ns3, *cloud_ns3_pc);
   pcl::toPCLPointCloud2(*cloud_livox_m, *cloud_livox_pc);
-  //pcl::toPCLPointCloud2(*merge_cloud, *merge_cloud_pc1);
-
 
   pcl::concatenatePointCloud(*cloud_ns1_pc, *cloud_ns2_pc, *merge_cloud_pc1);
   pcl::concatenatePointCloud(*cloud_ns3_pc, *merge_cloud_pc1, *merge_cloud_pc2);
@@ -151,6 +134,7 @@ void cloud_cb5 (const sensor_msgs::PointCloud2ConstPtr& cloud_msg5)
   sensor_msgs::PointCloud2 output; 
   pcl_conversions::fromPCL(*merge_cloud_pc3, output);
   pub.publish(output);
+    
   //delete merge_cloud;
   delete cloud_ns1_pc;
   delete cloud_ns2_pc;
